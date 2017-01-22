fb978607f4c7c44c2cf31b6b0b91b5ff97d4830a
JosephLivengood <JoeyLivengood@gmail.com>
2017-01-22 00:39:13 -0500

    Test runner impli with chai/mocha/chai-http

---
+++ test/test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const assert = require('chai').assert;
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Tests', function() {
+    describe('Mocha & Chai', function() {
+        it('should be working perfectly', function() {
+            assert.equal(1,1);
+        });
+    });
+    describe('Chai HTTP', function() {
+        it('should be working perfectly', function(done) {
+            chai.request(server)
+              .get('/')
+              .end(function(err, res) {
+                assert.equal(res.status, 200);
+                done();
+              });
+        })
+    })
+});
